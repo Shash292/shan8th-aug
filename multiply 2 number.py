9f56f7da6a5d2ad435bffcf795eaa4de475cc036
Shash292 <sbadoniya2611@gmail.com>
2025-08-05 17:12:36 +0530

    added python program

---
+++ multiply 2 number.py
@@ -0,0 +1,11 @@
+# Program to multiply two numbers
+
+# Input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Multiplication
+result = num1 * num2
+
+# Display the result
+print(f"The product of {num1} and {num2} is {result}")
